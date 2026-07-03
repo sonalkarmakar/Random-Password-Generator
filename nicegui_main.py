3149185ced40cd7ef8081c4e654f2161cd802971
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-07-03 11:49:42 +0530

    Finalised the layout

--- nicegui_main.py
+++ nicegui_main.py
@@ -1,45 +1,53 @@
 from nicegui import ui
 from random import choice
 
-from src.defined import author_details, icons
+from src.defined import author_details, icons, param_sliders
+
+param_input: dict[str, int] = {}
 
 # Create UI Elements
 with ui.column(align_items="center").classes("w-full"):
-	ui.html("<h3>Random Password Generator</h3>")
-	with ui.card(align_items="center"):
-		ui.html("<h4>Generate a Random Password</h4>")
+	ui.html("<center><h3>Random Password Generator</h3></center>")
+	with ui.card(align_items="center").classes("rounded-3xl"):
+		ui.html("<center><h4>Generate a Random Password</h4></center>")
 
 		with ui.row(align_items="center").classes("w-full"):
 			ui.label("Password Length")
 			ui.space()
 			ui.button(color="flat", icon=f"o_{icons['randomiser_icons'][0]}").props("flat round dense")
 
-		slider1 = ui.slider(min=1, max=16, value=1).props("label-always").classes("w-[97%]")
+		passwd_len = ui.slider(min=1, max=16, value=1).props("label-always").classes("w-[97%]")
 
-		with ui.card(align_items="center").classes("no-shadow border border-gray-300 w-full"):
-			with ui.row(align_items="center").classes("w-full"):
+		with ui.card(align_items="center").classes("w-full no-shadow rounded-3xl border border-gray-300"):
+			with ui.row(align_items="center").classes("w-full gap-2"):
 				ui.label("Valid Characters")
 				ui.space()
-				ui.button(color="flat", icon="o_refresh").props("flat round dense")
-				ui.button(color="flat", icon=f"o_{icons['randomiser_icons'][0]}").props("flat round dense")
+				ui.button(color="flat", icon="o_refresh").props("flat round dense padding='xs'")
+				ui.button(color="flat", icon=f"o_{icons['randomiser_icons'][0]}").props("flat round dense padding='xs'")
 
 			ui.separator()
 
-			with ui.row(align_items="center").classes("w-full"):
-				ui.markdown("Number of Uppercase characters [`A`, `B`, `C`, ... `Z`]")
-				ui.space()
-				# NiceGUI doesn't support new Material icons like "ifl"
-				ui.button(color="flat", icon=f"o_{icons['randomiser_icons'][0]}").props("flat round dense")
+			for k, v in param_sliders.items():
+				with ui.row(align_items="center").classes("w-full"):
+					ui.markdown(param_sliders[k]['label'])
+					ui.space()
+					# NiceGUI doesn't support new Material icons like "ifl"
+					ui.button(color="flat", icon=f"o_{icons['randomiser_icons'][0]}").props("flat round dense padding='xs'")
 
-			slider2 = ui.slider(min=1, max=16, value=1).props("label-always").classes("w-[97%]")
+				slider = ui.slider(
+					min=param_sliders[k]['min_val'],
+					max=param_sliders[k]['max_val'],
+					value=param_sliders[k]['min_val']
+				).props("label-always").classes("w-[97%]")
 
-			with ui.row(align_items="center").classes("w-full"):
-				ui.markdown("Number of Lowercase characters [`a`, `b`, `c`, ... `z`]")
-				ui.space()
-				# NiceGUI doesn't support new Material icons like "ifl"
-				ui.button(color="flat", icon=f"o_{icons['randomiser_icons'][0]}").props("flat round dense")
+				# print(type(slider.value)) shows <class 'int'>, but linter detects it as float | None
+				assert isinstance(slider.value, int) # Eliminate float and None types
+				param_input.update({k: slider.value})
 
-			slider3 = ui.slider(min=1, max=16, value=1).props("label-always").classes("w-[97%]")
+		with ui.row(align_items="center", wrap=False).classes("w-full gap-2"):
+			ui.button(text="Generate").props("no-caps no-wrap rounded")
+			ui.input(placeholder="Your Randomly Generated Password").props("outlined rounded dense input-class='text-center'").classes("w-full")
+			ui.button(color="flat", icon=f"o_{icons['content_copy']}").props("flat round dense")
 
 with ui.footer():
 	ui.space()
