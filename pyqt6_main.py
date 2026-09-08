2568b9094739d49ad8486d9d06897216d22fe401
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-09-08 16:49:29 +0530

    Created project skeleton

---
+++ pyqt6_main.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.uic.load_ui import loadUi
+
+
+class MainWindow(QMainWindow):
+	def __init__(self) -> None:
+		super().__init__()
+		loadUi("ui/main_window.ui", self)
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+
+	try:
+		with open("assets/styles.qss", "r") as f:
+			app.setStyleSheet(f.read())
+	except FileNotFoundError:
+		print("Style sheet not found! Running with default theme.")
+
+	window = MainWindow()
+	window.show()
+
+	app.exec()
