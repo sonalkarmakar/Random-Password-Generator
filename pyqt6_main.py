099bf442dda259b87ba39955b1171624bd4bdc19
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-09-22 11:00:31 +0530

    Added layouts on the guidelines tab

--- pyqt6_main.py
+++ pyqt6_main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow  #, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget  #, QWidget
 from PyQt6.uic.load_ui import loadUi
 
 
@@ -8,6 +8,9 @@ class MainWindow(QMainWindow):
 	def __init__(self) -> None:
 		super().__init__()
 		loadUi("ui/main_window.ui", self)
+		self.CreatePasswdScrollArea.setVisible(self.CreatePasswdHeaderButton.isChecked())
+		self.MaintPasswdScrollArea.setVisible(self.MaintPasswdHeaderButton.isChecked())
+
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
